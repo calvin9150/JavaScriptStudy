003d6e26f76b5dc09e71ff28c0b3dcae06e53cbf
calvin9150 <wonbae9150@gmail.com>
2020-10-27 00:00:40 +0900

    move_folder

---
+++ vanillaJS/index.js
@@ -0,0 +1,32 @@
+alert('Im Working. Im JS. Im Beautiful. Im Worth it');
+/*
+const what = "jang";
+console.log(what);
+*/
+/*
+Boolean
+const wat = true;
+*/
+/*
+Number
+const wat = 66;
+*/
+/*
+const wat = 55.1;
+*/
+
+const JangInfo = {
+    name : "jang",
+    age : 28,
+    gender : "male",
+
+
+}
+console.log(JangInfo.name);
+
+function sayHello(name, age){
+    console.log(`Hello!`, name, "you have", age, "years of age");
+}
+
+sayHello("WonBae", 28);
+console.log("Hi!")
