1b4e24ee60febed2ab49a988a9c83163d9ddcae3
calvin9150 <wonbae9150@gmail.com>
2021-04-03 23:36:39 +0900

    babel, webpack

--- zerocho_React_03/webpack.config.js
+++ zerocho_React_03/webpack.config.js
@@ -1,1 +1,32 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+  name: "wordrelay-setting",
+  mode: "development", // 실서비스 : production
+  devtool: "eval", // 빠르게 하겠다는 뜻
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  entry: {
+    app: ["./client.jsx"],
+  }, // 입력
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        option: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  }, // 출력
+};
