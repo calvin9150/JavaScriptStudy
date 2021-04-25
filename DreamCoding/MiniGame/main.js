728f5bbf3d37d578e0f69af23dc5cbb89bd32125
calvin9150 <wonbae9150@gmail.com>
2021-04-26 04:10:31 +0900

    Implement Timer and StopBtn

--- DreamCoding/MiniGame/main.js
+++ DreamCoding/MiniGame/main.js
@@ -1,10 +1,91 @@
 const gameBtn = document.querySelector(".game__button");
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
-const carrotSize = 80;
+const gameTimer = document.querySelector(".game__timer");
+const gameScore = document.querySelector(".game__score");
 
-function makeCarrots() {
-  field.innerText = `<button class="carrot"><img src="img/carrot.png" /></button>`;
+const popUp = document.querySelector(".pop-up");
+const popUpText = document.querySelector(".pop-up__message");
+const popUpRefresh = document.querySelector(".pop-up__refresh");
+
+const carrotSize = 80;
+const carrotCount = 5;
+const bugCount = 5;
+const gameDurationSec = 5;
+
+let started = false;
+let score = 0;
+let timer;
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
+
+function stopGame() {
+  stopGameTimer();
+  hideGameButton();
+  showPopUpWithText("REPLAY?");
+}
+
+function startGameTimer() {
+  let remainingTimeSec = gameDurationSec;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval;
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function stopGameTimer() {
+  clearInterval(timer);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+gameBtn.addEventListener("click", () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function showStopButton() {
+  const icon = gameBtn.querySelector(".fa-play");
+  icon.classList.add("fa-stop");
+  icon.classList.add("fa-play");
+}
+
+function hideGameButton() {
+  gameBtn.style.visibility = "hidden";
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = "visible";
+  gameScore.style.visibility = "visible";
+}
+
+function showPopUpWithText(text) {
+  popUpText.innerText = text;
+  popUp.classList.remove("pop-up--hide");
+}
+
+function initGame() {
+  field.innerHTML = "";
+  gameScore.innerText = carrotCount;
+  addItem("carrot", carrotCount, "img/carrot.png");
+  addItem("bug", bugCount, "img/bug.png");
 }
 
 function addItem(className, count, imgPath) {
@@ -28,10 +109,3 @@ function addItem(className, count, imgPath) {
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
-
-function initGame() {
-  addItem("carrot", 5, "img/carrot.png");
-  addItem("bug", 5, "img/bug.png");
-}
-
-initGame();
